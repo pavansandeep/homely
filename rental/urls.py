42155fb143ec52effe03ca29d5cc1fb7727fb738
pchitrapu <pchitrapu@apple.com>
2018-02-17 16:55:29 -0800

    Updated comments in rental/urls.py

--- rental/urls.py
+++ rental/urls.py
@@ -10,20 +10,24 @@ urlpatterns = [
     # Owner Accounts. This is for both POST and GET methods.
     url(r'^owners$', views.OwnerList.as_view(), name='owner'),
 
-    # GET, PUT, PATCH, DELETE methods on owner account.
+    # GET, PUT, PATCH, DELETE methods on owner account. Cannot delete owner account if they own any properties.
     url(r'^owners/(?P<pk>[0-9]+)$', views.OwnerDetail.as_view(), name='owner-detail'),
 
     # Renter Accounts. This is for both POST and GET methods.
     url(r'^renters$', views.RenterList.as_view(), name='renter'),
 
-    # GET, PUT, PATCH, DELETE methods on renter account. Cannot delete renter account if he has any properties reserved.
+    # GET, PUT, PATCH, DELETE methods on renter account. Cannot delete renter account if they have any properties
+    #  reserved.
     url(r'^renters/(?P<pk>[0-9]+)$', views.RenterDetail.as_view(), name='renter-detail'),
 
-    # Create properties for a owner. Assuming only owners can add properties. POST and GET of all properties for a owner.
+    # Create properties for a owner. Assuming only owners can add properties. POST and GET of all properties for a
+    #  owner.
     url(r'^owners/(?P<pk>[0-9]+)/properties$', views.OwnerPropertyList.as_view(), name='ownerproperty'),
 
-    # GET, PUT, PATCH, DELETE methods on properties by a owner.
-    url(r'^owners/(?P<pk>[0-9]+)/properties/(?P<pk>[0-9]+)$', views.OwnerPropertyDetail.as_view(), name='ownerproperty-detail'),
+    # GET, PUT, PATCH, DELETE methods on properties by a owner. Cannot delete a property if there is an existing
+    #  reservation.
+    url(r'^owners/(?P<pk>[0-9]+)/properties/(?P<pk>[0-9]+)$', views.OwnerPropertyDetail.as_view(),
+        name='ownerproperty-detail'),
 
     # API to find all properties, dates can be passed in query params to find list of all available properties in a date
     # range.
@@ -32,7 +36,8 @@ urlpatterns = [
     # API for GET, PUT, PATCH, DELETE given an id
     url(r'^properties/(?P<pk>[0-9]+)$', views.PropertyDetail.as_view(), name='property-detail'),
 
-    # API to search all properties and can pass any property related fields as query params
+    # API to search all properties and can pass any property related fields as query params. Like find available
+    #  properties based on start_date, end_date passed as query params.
     url(r'^search/properties$', views.SearchProperties.as_view(), name='search-properties'),
 
     # API to create and get reservations.
